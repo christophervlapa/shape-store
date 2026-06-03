cebb1925437f61e41332ac2a1ec2b84c49a696af
Christopher V. Lapa <chris.lapa@pm.me>
2026-06-03 12:22:23 +1000

    Initial commit, set up vite react project, add tailwind, react router, shadcn, cards, main store

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+// import { Welcome } from "../welcome/welcome";
+import {Store} from "../pages/store";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "The Shape Store" },
+    { name: "description", content: "Vite + React + React Router + ShadCD" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Store />;
 }
